cd0a4bf9394f0eed6696fe73cd05de5db0a39fb1
Chaiphan Khamphan <Chaiphan.k@ku.th>
2025-09-16 15:18:08 +0700

    week08

--- assignments/week08/quiz-encapsulation.py
+++ assignments/week08/quiz-encapsulation.py
@@ -6,4 +6,26 @@ Methods to calculate area (getArea()) and perimeter getPerimeter())
 A method to check if it's a square (isSquare())
 
 """
+
+class rectangle:
+    def __init__(self,lenght,width):
+        self.__lenght = lenght
+        self.__width = width
+
+    def getArea(self):
+        area = self.__lenght * self.__width
+        return f"Area={area}"
     
+    def getperamiter(self):
+        peramiter = 2* (self.__lenght + self.__width)
+        return f"peramiter = {peramiter}"
+    
+    def isSquare(self):
+        if self.__width == self.__lenght :
+            return F"This rectangle is square idiot!"
+        else:
+            return f"This rectangle is Not square idiot!"
+rectangle = rectangle(10,15)
+print(rectangle.getArea())
+print(rectangle.getperamiter())
+print(rectangle.isSquare())
--- assignments/week08/quiz-inheritance.py
+++ assignments/week08/quiz-inheritance.py
@@ -6,3 +6,22 @@ Create a class hierarchy:
     Implement a method get_info() in both classes
 
 """
+
+class vehicle:
+    def __init__(self,brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        
+    def getinfo(self,brand, model, year):
+        return f"This car is{self.brand} the module is {self.model}! ({self.year})"
+    
+class car(vehicle):
+    def __init__(self, brand, model, year, number_of_doors):
+        super().__init__(brand, model, year)
+        self.number_of_doors = number_of_doors
+    
+    def getinfo(self):
+        return f"This is{self.brand} the module is form {self.model}! ({self.year}) have{self.number_of_doors}doors!"
+Minecart = car(" Minecart" , "Minecraft", " 2014 ", " 4 ")
+print(Minecart.getinfo())
